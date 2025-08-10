b9d505f538eb34cfdd673e074839bd6c5224030f
Anastasiia Lukianets <lukianets.anastasiia@gmail.com>
2025-08-10 20:58:38 +0200

    Defining Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home";
+
+// an array of route definition objects
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
