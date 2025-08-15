6aa6f9e8d1dfb9b34717255d9b6fdccd7e58b8ec
Anastasiia <lukianets.anastasiia@gmail.com>
2025-08-15 19:21:10 +0200

    Navigation Links - NavLink

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 import classes from "./MainNavigation.module.css";
 
@@ -8,10 +9,25 @@ function MainNavigation() {
       <nav>
         <ul className={classes.list}>
           <li>
-            <Link to="/">Home</Link>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              to="/products"
+            >
+              Products
+            </NavLink>
           </li>
         </ul>
       </nav>
